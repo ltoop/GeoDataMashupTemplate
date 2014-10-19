c9fba281f52c58164f5c2022e1b4115abc976a93
Luke Toop <luke@luketoop.com>
2014-10-19 12:52:09 +1100

    Initial Commit

---
+++ GeoDataMashupTemplate/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GeoDataMashupTemplate
+//
+//  Created by Luke Toop on 19/10/2014.
+//  Copyright (c) 2014 luketoop.com. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
